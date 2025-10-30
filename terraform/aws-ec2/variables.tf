6d12506484843d57450e2f6d81c4cc16f6ae54a4
iso-st3ph <stephonmskipper@gmail.com>
2025-10-29 21:23:32 -0400

    fix(terraform): de-duplicate variables; keep only in variables.tf

---
+++ terraform/aws-ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  type    = string
+  default = null
+}
+
+variable "tags" {
+  type    = map(string)
+  default = { project = "homelab-devops" }
+}
