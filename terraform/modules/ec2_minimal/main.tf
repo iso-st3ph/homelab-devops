9cbcb9aab007267e724e89333a17c2056f7e8766
iso-st3ph <stephonmskipper@gmail.com>
2025-10-29 21:51:38 -0400

    updates

--- terraform/modules/ec2_minimal/main.tf
+++ terraform/modules/ec2_minimal/main.tf
@@ -2,19 +2,29 @@ provider "aws" {
   region = var.region
 }
 
-# Safe-by-default: resources commented until you're ready to deploy.
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
-#   owners      = ["099720109477"] # Canonical
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
-#   }
-# }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
 
-# resource "aws_instance" "this" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-#   tags          = merge({ Name = "homelab-ec2" }, var.tags)
-# }
+resource "aws_instance" "this" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  tags = merge(
+    { Name = "homelab-ec2" },
+    var.tags
+  )
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}
